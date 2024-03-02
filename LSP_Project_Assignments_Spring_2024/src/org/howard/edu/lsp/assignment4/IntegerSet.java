0d3d27d8d8a64f846746723f9797a983444b4655
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-01 21:14:49 -0500

    Implemented union and intersection methods

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -21,8 +21,49 @@ public class IntegerSet {
 	
 	// Clears the internal representation of the set
 	public void clear() {
-		set.clear()
+		set.clear();
 	};
+	
+	// Returns the length of the set
+    public int length() {
+        return set.size();
+    }
+
+    // Returns the largest item in the set; Throws an IntegerSetException if the set is empty
+    public int largest() {
+        if (set.isEmpty()) {
+            throw new IntegerSetException("Set is empty");
+        }
+        return set.stream().max(Integer::compare).orElseThrow();
+    }
+
+    // Returns the smallest item in the set; Throws an IntegerSetException if the set is empty
+    public int smallest() {
+        if (set.isEmpty()) {
+            throw new IntegerSetException("Set is empty");
+        }
+        return set.stream().min(Integer::compare).orElseThrow();
+    }
+    
+    // Adds an item to the set or does nothing if already there
+    public void add(int item) {
+        if (!set.contains(item)) {
+            set.add(item);
+        }
+    }
+
+    // Removes an item from the set or does nothing if not there
+    public void remove(int item) {
+        set.remove(Integer.valueOf(item));
+    }
+
+    
+    public static class IntegerSetException extends RuntimeException {
+        public IntegerSetException(String message) {
+            super(message);
+        }
+    }
+
 
 
 }
