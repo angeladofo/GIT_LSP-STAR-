12ea33cde27f802244d7f0edae7feffd1492d270
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 19:53:40 -0400

    getting rid of any spaces

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -2,8 +2,10 @@ package org.howard.edu.lsp.midterm.question1;
 
 public class SecurityOps {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static String encrypt(String text) {
+		
+		String my_text =m text.replaceAll("[\\s\\p{Punct}]", "");
+		
 		StringBuilder evenLets = new StringBuilder();
         StringBuilder oddLets = new StringBuilder();
 
