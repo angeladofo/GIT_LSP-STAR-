f388b70eb4069c976a5ddeba7308225afb32aa5c
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 19:48:33 -0400

    Stringbuilders that ill store the even and odd elements

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -4,6 +4,8 @@ public class SecurityOps {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		StringBuilder evenLets = new StringBuilder();
+        StringBuilder oddLets = new StringBuilder();
 
 	}
 
