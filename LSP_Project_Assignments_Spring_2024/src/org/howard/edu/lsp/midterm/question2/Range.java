cb3e591da3ca230965a5b576e83970d63e976a73
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 20:09:09 -0400

    contains boolean

---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public interface Range {
+	/**
+	 * Returns true when the range contains the val
+	 * @param value t
+	 * @return true when value is within range
+	 */
+	boolean contains(int value);
+
+
+
+}
