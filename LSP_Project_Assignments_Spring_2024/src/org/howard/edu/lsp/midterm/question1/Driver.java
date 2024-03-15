527c1ae97ab99c06f29cbea3c5ec6bdeca408ea1
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 20:03:22 -0400

    Printing message

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -3,7 +3,8 @@ package org.howard.edu.lsp.midterm.question1;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String hiddenText = SecurityOps.encrypt("I love CSCI363");
+        System.out.println(hiddenText);
 
 	}
 
