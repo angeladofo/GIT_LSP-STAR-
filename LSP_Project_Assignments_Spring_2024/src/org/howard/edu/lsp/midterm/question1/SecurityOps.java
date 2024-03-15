4035952b28cf78af9fbebe5679e195db69fb1cd9
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 20:25:39 -0400

    adding my notes and javadocs

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -1,23 +1,35 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * This class handles encrypting text.
+ */
 public class SecurityOps {
+	/** It rearranges the characters so it looks like a secret code!
+     *
+     * @param text the text to be encrypted
+     * @return the encrypted text, all scrambled up and ready for secrecy
+     */
 
 	public static String encrypt(String text) {
-		
+		//removing all spacing etc.
 		String myText = text.replaceAll("[\\s\\p{Punct}]", "");
 		
 		StringBuilder evenLets = new StringBuilder();
         StringBuilder oddLets = new StringBuilder();
         
+        //looping through the text appending them respectively
         for (int i = 0; i < myText.length(); i++) {
             char ch = myText.charAt(i);
             
+            //even goes to evenLets odd goes to oddLets
             if (i % 2 == 0) {
                 evenLets.append(ch);
             } else { 
                 oddLets.append(ch);
             }
         }
+        //combining the two
+        return evenLets.toString() + oddLets.toString();
 
 	}
 
