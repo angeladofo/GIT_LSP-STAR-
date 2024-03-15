c39fb7f816dcfc75bac7aa5fb061f22c43c2148c
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-03-14 20:37:09 -0400

    creating testing for my implementation

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
@@ -1,5 +1,9 @@
 package org.howard.edu.lsp.midterm.question2;
 
 public class RangeTester {
-
+	public static void main(String[] args) {
+        // Creating two integer ranges for testing
+        IntegerRange range1 = new IntegerRange(5, 10);
+        IntegerRange range2 = new IntegerRange(4, 14);
+	}
 }
