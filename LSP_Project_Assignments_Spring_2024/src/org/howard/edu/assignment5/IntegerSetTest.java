788c25593d82d97c81b85a3065b03100da5e6271
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-04-04 23:09:58 -0400

    testing error handling

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/assignment5/IntegerSetTest.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/assignment5/IntegerSetTest.java
@@ -1,6 +1,8 @@
 package org.howard.edu.assignment5;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.howard.edu.assignment5.IntegerSet.IntegerSetException;
 import org.junit.jupiter.api.Test;
 
 
@@ -32,6 +34,45 @@ class IntegerSetTest {
 
         assertTrue(set1.equals(set2));
     }
+    
+    @Test
+    public void testContains() {
+        IntegerSet set = new IntegerSet();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        assertTrue(set.contains(2));
+        assertFalse(set.contains(4));
+    }
+    
+    @Test
+    public void testLargestWithNonEmptySet() {
+        IntegerSet set = new IntegerSet();
+
+        set.add(5);
+        set.add(10);
+        set.add(3);
+
+        try {
+            assertEquals(10, set.largest());
+        } catch (IntegerSetException e) {
+            fail("Should not throw an exception for non-empty set");
+        }
+    }
+    
+    @Test
+    public void testLargestWithEmptySet() {
+        IntegerSet set = new IntegerSet();
+
+        try {
+            set.largest();
+            fail("Should throw an exception for empty set");
+        } catch (IntegerSetException e) {
+            // Expected behavior
+        }
+    
 
 }
 
