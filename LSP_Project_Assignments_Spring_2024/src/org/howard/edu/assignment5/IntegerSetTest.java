11e6dc2b3f5b0cec41c1c8e61c475c7c6347acf3
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-04-04 23:09:06 -0400

    test for contains and largest method

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/assignment5/IntegerSetTest.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/assignment5/IntegerSetTest.java
@@ -1,14 +1,38 @@
 package org.howard.edu.assignment5;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
 
 class IntegerSetTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testGetLength() {
+        IntegerSet set = new IntegerSet();
+        set.add(5);
+        set.add(10);
+        set.add(3);
+        assertEquals(3, set.length());
+    }
+
+	
+	
+    @Test
+    public void testEquality() {
+        IntegerSet set1 = new IntegerSet();
+        IntegerSet set2 = new IntegerSet();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+
+        assertTrue(set1.equals(set2));
+    }
 
 }
+
+
