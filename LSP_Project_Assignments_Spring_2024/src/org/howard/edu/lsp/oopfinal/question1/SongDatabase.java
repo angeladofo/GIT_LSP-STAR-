debd3960c1068a86b2d5996278f75713c3ad5786
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-05-02 20:09:14 -0400

    question 1

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question1/SongDatabase.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question1/SongDatabase.java
@@ -1,5 +1,34 @@
 package org.howard.edu.lsp.oopfinal.question1;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class SongDatabase {
+    // The map that links genres to a set of song titles
+    private Map<String, Set<String>> map = new HashMap<>();
 
+    /* Add a song title to a genre */
+    public void addSong(String genre, String songTitle) {
+        // If the genre doesn't exist in the map, create a new HashSet
+        map.putIfAbsent(genre, new HashSet<>());
+        // Add the song to the set for the given genre
+        map.get(genre).add(songTitle);
+    }
+
+    /* Return genre, i.e., Jazz, given a song title */
+    public String getGenreOfSong(String songTitle) {
+        // Iterate over the map to find the genre containing the song title
+        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
+            if (entry.getValue().contains(songTitle)) {
+                return entry.getKey();
+            }
+        }
+        return null; // Return null if the song title is not found in any genre
+    }
+
+    /* Return the Set that contains all songs for a genre */
+    public Set<String> getSongs(String genre) {
+        return map.get(genre); // Return the set for the given genre
+    }
 }
--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question1/SongsDatabaseTest.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question1/SongsDatabaseTest.java
@@ -1,14 +1,45 @@
 package org.howard.edu.lsp.oopfinal.question1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+import java.util.Set;
 
-class SongsDatabaseTest {
+public class SongsDatabaseTest {
+    private SongDatabase db;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void setUp() {
+        db = new SongDatabase();
+    }
 
+    @Test
+    public void testAddSong() {
+        db.addSong("Rap", "Savage");
+        Set<String> rapSongs = db.getSongs("Rap");
+        assertNotNull(rapSongs); 
+        assertTrue(rapSongs.contains("Savage")); 
+    }
+
+    @Test
+    public void testGetGenreOfSong() {
+        db.addSong("Country", "Sweet Alabama");
+        String genre = db.getGenreOfSong("Sweet Alabama");
+        assertEquals("Country", genre); // Check if the correct genre is returned
+
+        genre = db.getGenreOfSong("Unknown Song");
+        assertNull(genre); // Should return null for a non-existent song
+    }
+
+    @Test
+    public void testGetSongs() {
+        db.addSong("Jazz", "Always There");
+        db.addSong("Jazz", "Take Five");
+        Set<String> jazzSongs = db.getSongs("Jazz");
+        assertNotNull(jazzSongs); // The set should not be null
+        assertEquals(2, jazzSongs.size()); // The set should contain two songs
+        assertTrue(jazzSongs.contains("Always There")); // Check if "Always There" is in the set
+        assertTrue(jazzSongs.contains("Take Five")); // Check if "Take Five" is in the set
+    }
 }
