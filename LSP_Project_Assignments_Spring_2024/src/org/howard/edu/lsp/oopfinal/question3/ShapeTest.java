af73bfd9985ce4b8f2e813385486bdd734fa0b45
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-05-02 20:35:41 -0400

    test

---
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/oopfinal/question3/ShapeTest.java
@@ -0,0 +1,29 @@
+package org.howard.edu.lsp.oopfinal.question3;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
+
+public class ShapeTest {
+	private FactShape factShape;
+	
+	
+	 @Before
+	    public void setUp() {
+	        factShape = new FactShape();
+	    }
+	 
+	 @Test
+	    public void testCreateCircle() {
+	        Shape circle = factShape.createShape("Circle");
+	        assertNotNull(circle); //  Circle instance is created
+	        assertTrue(circle instanceof Circle); // Check the instance type is Circle
+	 }
+	 
+	 @Test
+	    public void testCreateRectangle() {
+	        Shape rectangle = factShape.createShape("Rectangle");
+	        assertNotNull(rectangle); // Rectangle instance is created
+	        assertTrue(rectangle instanceof Rectangle); // Check the instance type is Rectangle
+	 }
+}
